22dfde86776cce9b5b422464159d57cc98cd1b9f
atracy23 <111539642+atracy23@users.noreply.github.com>
2023-10-07 08:48:46 -0500

    Removed unnecessary import library

---
+++ MoffatBay/Counter.java
@@ -0,0 +1,19 @@
+package com.moffat.bay.util;
+
+public class Counter {
+
+	private int counter = 0;
+	
+	public void increment() {
+		counter++;
+		
+	}
+	
+	public void decrement() {
+		counter--;
+	}
+	
+	public int getCounter() {
+		return counter;
+	}
+}
