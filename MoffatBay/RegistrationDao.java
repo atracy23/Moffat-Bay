2c87bfdcdee3aa34c6ad9d2423ea5ee79669749d
atracy23 <111539642+atracy23@users.noreply.github.com>
2023-10-07 08:54:21 -0500

    Added suggested changes
    Added new query to reservation to obtain all room sizes available for requested time frame

---
+++ MoffatBay/RegistrationDao.java
@@ -0,0 +1,86 @@
+package com.moffat.bay.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import com.moffat.bay.model.RegistrationBean;
+import com.moffat.bay.util.*;
+
+
+public class RegistrationDao {
+	
+	public RegistrationDao() {
+		
+	}
+	
+	public RegistrationBean getRegisterInfo(String firstName, String lastName, String email, String phoneNum, String password) throws ClassNotFoundException, SQLException{
+		
+		//Creating objects to be returned depending on whether user was updated in database table or not
+		RegistrationBean register = null;
+		RegistrationBean newUser = new RegistrationBean(firstName, lastName, email, phoneNum, password);
+		
+		Connection dbConn = null;
+		PreparedStatement emailStatement = null;
+		ResultSet emailResult = null;
+		PreparedStatement createUserStatement = null;
+		
+		try {
+			
+			dbConn = dbConnection();
+			
+			String getEmailQuery = "SELECT lastName FROM users WHERE email = ?";
+			
+			emailStatement = dbConn.prepareStatement(getEmailQuery);
+			emailStatement.setString(1, email);
+			emailResult = emailStatement.executeQuery();
+			
+			//if email does exist in db user table
+			if(emailResult.next()) {
+				return register;
+			} 
+
+			//email does not exist currently - adding user info to user table
+			String encryptedPassword = PasswordHash.hash(password);
+			int userResult;
+			
+			String createUser = "INSERT INTO users (firstName, lastName, email, phoneNum, password)" + "VALUES(?, ?, ?, ?, ?)";
+			createUserStatement = dbConn.prepareStatement(createUser);
+			createUserStatement.setString(1, firstName);
+			createUserStatement.setString(2, lastName);
+			createUserStatement.setString(3, email);
+			createUserStatement.setString(4, phoneNum);
+			createUserStatement.setString(5, encryptedPassword);
+			userResult = createUserStatement.executeUpdate();
+			
+			return newUser;
+			
+		}finally {
+			if(emailResult != null) {
+				emailResult.close();
+			}
+			if(emailStatement !=null) {
+				emailStatement.close();
+			}
+			if(createUserStatement != null) {
+				createUserStatement.close();
+			}
+			if(dbConn != null) {
+				dbConn.close();
+			}
+		}
+	}
+	
+	public Connection dbConnection() throws ClassNotFoundException, SQLException{
+        String dbUrl = "jdbc:mysql://localhost:3306/moffat_bay";
+        String dbUsername = "root";
+        String dbPassword = "root";
+
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		Connection conn = DriverManager.getConnection(dbUrl, dbUsername, dbPassword);
+
+        return conn;
+	}
+}
---
+++ MoffatBay/ReservationDao.java
@@ -0,0 +1,127 @@
+/* Green Team MoffatBay Lodge Project
+	 Bellevue University
+	 October 2023
+	 Professor Sue Sampson
+	 Team members:	Ron Stewart
+									Rashmi Sathiyanarayanan
+									Joseph Youskievicz
+									Angie Tracy
+*/
+
+package com.moffat.bay.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import com.moffat.bay.model.*;
+
+public class ReservationDao {
+	
+	public ReservationDao() {
+		
+	}
+	
+	public ArrayList<Integer> fetchRoom(ReservationBean reservationBean) throws SQLException, ClassNotFoundException {
+		Connection dbConn = null;
+		PreparedStatement roomStatement = null;
+		ResultSet roomResult = null;
+		
+		System.out.println("Entered Dao");
+		
+		try {
+			
+			dbConn = dbConnection();
+			
+			ArrayList<Integer> userResult = new ArrayList<Integer>();
+			
+			String fetchAvailableRoom = "SELECT roomNum FROM rooms WHERE currentDate BETWEEN ? AND ? AND roomSize = ? AND maxOccup >= ? AND available = '1'";
+			roomStatement = dbConn.prepareStatement(fetchAvailableRoom);
+
+			roomStatement.setDate(1, reservationBean.getInDate());
+			roomStatement.setDate(2, reservationBean.getOutDate());
+			roomStatement.setString(3, reservationBean.getRoomSize());
+			roomStatement.setInt(4, reservationBean.getNumGuests());
+
+			roomResult = roomStatement.executeQuery();
+			
+			while (roomResult.next()) {
+	        	userResult.add(roomResult.getInt("roomNum")); // Get the roomNum from the ResultSet
+	        }
+	        
+	        System.out.println("Room number retrieved: "+userResult);
+	        return userResult;
+
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally {
+			if(roomStatement != null) {
+				roomStatement.close();
+			}
+			if(dbConn != null) {
+				dbConn.close();
+			}
+			
+		}
+		return null;
+
+	}
+	
+	public int updateRooms(ReservationBean reservationBean) throws SQLException, ClassNotFoundException {
+		Connection dbConn = null;
+		PreparedStatement roomStatement = null;
+		ResultSet roomResult = null;
+		
+		try {
+			
+			dbConn = dbConnection();
+			
+			int roomUpdateRows;
+			
+			String updateRooms = "UPDATE rooms SET available = 'false' WHERE roomNum = ? AND currentDate BETWEEN ? AND ? "; 
+			roomStatement = dbConn.prepareStatement(updateRooms);
+			roomStatement.setInt(1, reservationBean.getRoomNum());
+			roomStatement.setDate(2, reservationBean.getInDate());
+			roomStatement.setDate(3, reservationBean.getOutDate());
+			roomUpdateRows = roomStatement.executeUpdate();
+			
+			System.out.println("Room table updated with number rows "+ roomUpdateRows);	
+			
+			return roomUpdateRows;
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally {
+			if(roomStatement != null) {
+				roomStatement.close();
+			}
+			if(dbConn != null) {
+				dbConn.close();
+			}
+			
+		}
+		
+		return 0;
+	}
+	
+
+
+	
+	public Connection dbConnection() throws ClassNotFoundException, SQLException {
+        String dbUrl = "jdbc:mysql://localhost:3306/moffat_bay";
+        String dbUsername = "root";
+        String dbPassword = "Yogesh@05";
+
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		Connection conn = DriverManager.getConnection(dbUrl, dbUsername, dbPassword);
+
+        return conn;
+	}
+
+}
